347595cd305755a92b5928797fd02b9dbb6603f0
mzhao15 <39840210+mzhao15@users.noreply.github.com>
2018-07-13 22:17:56 -0400

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
 # script to compile and execute a C program
-declare -r color_start="\033["
-declare -r color_red="${color_start}0;31m"
-declare -r color_green="${color_start}0;32m"
-declare -r color_blue="${color_start}0;34m"
-declare -r color_norm="${color_start}0m"
 
 module load gcc
 gcc -o counting ./src/pharmacy_counting.c
