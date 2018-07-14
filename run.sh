1bfb6608a38e9732953a80d20191b966b835d6dc
mzhao15 <39840210+mzhao15@users.noreply.github.com>
2018-07-14 00:10:10 -0400

    Update run.sh

--- run.sh
+++ run.sh
@@ -2,6 +2,6 @@
 # script to compile and execute a C program
 
 module load gcc
-gcc -o counting ./src/pharmacy_counting.c
-./counting
-rm counting
+gcc -o counting.out ./src/pharmacy_counting.c
+./counting.out
+rm counting.out
